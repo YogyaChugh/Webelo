09d23f9434c844430fa0087215579d14aa596c91
YogyaChugh <yogyachugh.coder@gmail.com>
2025-07-11 21:49:28 +0530

    Updates....

--- src/nodes/document.cpp
+++ src/nodes/document.cpp
@@ -1,4 +1,6 @@
 #include "document.hpp"
+#include "../../include/nodes/document.hpp"
+
 #include "../../include/base.hpp"
 #include <optional>
 
@@ -17,4 +19,30 @@ Node::Node(){
 
 bool Node::hasChildNodes(){
     // TODO: Check if Nodelist childnodes is empty or not
+}
+
+
+Node* convert_nodes_to_node(std::initializer_list<std::variant<Node,DOMString>> nodes, Document* ownerDocument) {
+    Node* node = nullptr;
+    for (size_t i = 0; i < nodes.size(); i++) {
+        if (dynamic_cast<DOMString*>(nodes[i])) {
+            nodes[i] = Text(nodes[i]);
+            nodes[i].ownerDocument = ownerDocument;
+        }
+        else {
+            node = &(nodes[0]);
+        }
+    }
+    if (!node) {
+        node = new DocumentFragment();
+        node->ownerDocument = ownerDocument;
+        for (auto a: nodes) {
+            node
+        }
+    }
+}
+
+
+void ParentNode::prepend(std::initializer_list<std::variant<Node,DOMString>> nodes) {
+    convert_nodes_to_node(nodes, ownerDocument);
 }
